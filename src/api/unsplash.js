06ba7bea273e87562e8591a37f4cce647c267823
Avneet Singh Malhotra <avneet@snackmagic.com>
2023-11-29 19:56:14 +0530

    npm install with node v16.15.0

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: 'Client-ID YOUR_ACCESS_KEY'
+    Authorization: 'Client-ID 3443cafb2d8c7a1c8087dec65df0671e665b6c84d97ae426a46617f56ce5881c'
   }
 
 });
