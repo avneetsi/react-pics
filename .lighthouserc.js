fbc4e2629f719db8cde237e49c0e97ed7583bc1a
Avneet Singh Malhotra <avneet@snackmagic.com>
2023-12-13 20:23:17 +0530

    lighthouse assertion

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,5 +1,13 @@
 module.exports = {
   ci: {
+    assert: {
+      assertions: {
+        "categories:performance": ["warn", { minScore: 0.9 }],
+        "categories:accessibility": ["error", { minScore: 1 }],
+        "categories:best-practices": ["error", { minScore: 1 }],
+        "categories:seo": ["error", { minScore: 0.9 }],
+      },
+    },
     upload: {
       target: "temporary-public-storage",
     },
